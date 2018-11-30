3d503298d20cd97552f93181a84be5927e797968
fifionaLin <fifiona851024@gmail.com>
2018-11-30 12:07:12 +0800

    restruct random

--- config.py
+++ config.py
@@ -7,8 +7,19 @@ custumer_num = random.randint(2, 6)
 allocation = []
 Max = []
 for i in range(custumer_num):
-    allocation.append(random.sample(range(0, 4), resource_num))
-    Max.append(random.sample(range(5, 10), resource_num))
+    #allocation.append(random.sample(range(0, 4), resource_num))
+    #Max.append(random.sample(range(5, 10), resource_num))
+    allocation.append([])
+    Max.append([])
+
+    for j in range(resource_num):
+        tmp = random.randint(0, 4)
+        allocation[i].append(tmp)
+
+        if (tmp < 4):
+            Max[i].append(random.randint(tmp, tmp+5))
+        else:
+            Max[i].append(random.randint(tmp, 9))
+
 
 Bank_Avaliable = random.sample(range(1, 7), resource_num)
-
