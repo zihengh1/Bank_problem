2d1468f329d3dc531ff12cad2461ad301aa1e39d
fifionaLin <fifiona851024@gmail.com>
2018-12-04 10:56:11 +0800

    revise random

--- config.py
+++ config.py
@@ -20,10 +20,6 @@ for i in range(custumer_num):
         tmp = random.randint(0, 4)
         allocation[i].append(tmp)
 
-        #if (tmp < 4):
         Max[i].append(random.randint(tmp, tmp+5))
-        #else:
-            #Max[i].append(random.randint(tmp, 9))
-
 
 Bank_Avaliable = random.sample(range(1, 7), resource_num)
