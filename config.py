a60b2d817829fb497d1ca447571880aa9206f60d
Zi-Heng, Huang <105703031@nccu.edu.tw>
2018-12-12 11:22:55 +0800

    limit resource and costumer number

--- config.py
+++ config.py
@@ -2,24 +2,19 @@ import random
 import time
 
 random.seed(time)
-resource_num = random.randint(2, 5)
-custumer_num = random.randint(2, 6)
+resource_num = 3
+costumer_num = 4
 allocation = []
 Max = []
-name_data = ['Anna', 'Zoe', 'Tina', 'Joe', 'Peter', 'Mia', 'Bob', 'Andy', 'Nick', 'Dylan', 'Rachel', 'Sally', 'Bella', 'Lisa', 'Jenny', 'Wendy', 'Joseph', 'Robert', 'Tom', 'Amy', 'Justin', 'Jessica', 'Sunny', 'Raina', 'Daniel']
+name_data = ['Anna', 'Zoe', 'Tina', 'Joe']
 
-cus_name = random.sample(name_data, custumer_num)
-
-for i in range(custumer_num):
-    #allocation.append(random.sample(range(0, 4), resource_num))
-    #Max.append(random.sample(range(5, 10), resource_num))
+for i in range(costumer_num):
     allocation.append([])
     Max.append([])
 
     for j in range(resource_num):
-        tmp = random.randint(0, 4)
+        tmp = random.randint(0, 3)
         allocation[i].append(tmp)
+        Max[i].append(random.randint(3, 6))
 
-        Max[i].append(random.randint(tmp, tmp+5))
-
-Bank_Avaliable = random.sample(range(1, 7), resource_num)
+Bank_Avaliable = random.sample(range(3, 6), resource_num)
